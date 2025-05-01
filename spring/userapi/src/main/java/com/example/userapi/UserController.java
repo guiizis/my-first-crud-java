21040e8f4df8af0b5d1cf87fc9ee7c76ddb087ec
guiziis <guigamoda@hotmail.com>
2025-05-01 20:33:53 -0300

    add controller

---
+++ spring/userapi/src/main/java/com/example/userapi/UserController.java
@@ -0,0 +1,15 @@
+package com.example.userapi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+  @GetMapping
+  public String listUsers() {
+    return "here we should return users";
+  }
+}
