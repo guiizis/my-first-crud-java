13bec98dbdbbce5379027b4390f7bcef6a7c2071
guiziis <guigamoda@hotmail.com>
2025-05-01 20:40:25 -0300

    add model

---
+++ spring/userapi/src/main/java/com/example/userapi/User.java
@@ -0,0 +1,38 @@
+package com.example.userapi;
+
+public class User {
+
+    private Long id;
+    private String name;
+    private int age;
+
+    public User(Long id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long newId) {
+        this.id = newId;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String newName) {
+        this.name = newName;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int newAge) {
+        this.age = newAge;
+    }
+}
