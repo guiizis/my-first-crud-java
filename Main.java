63caa3e5d30ddbf9ae5079ade528fe80dac812a2
guiziis <guigamoda@hotmail.com>
2025-05-01 17:32:11 -0300

    add dogs registry

--- Main.java
+++ Main.java
@@ -3,72 +3,79 @@
 import java.util.List;
 import java.util.Scanner;
 import model.user.User;
-import user.UserRepository;
 
 public class Main {
-  public static void main(String[] args) {
-    List<User> users = UserRepository.loadUsers();
-    Scanner scanner = new Scanner(System.in);
-    boolean running = true;
 
-    while(running) {
-      System.out.println("\n CRUD Menu:");
-      System.out.println("1. Create User");
-      System.out.println("2. Read Users");
-      System.out.println("3. Update User");
-      System.out.println("4. Delete User");
-      System.out.println("5. Exit");
-      System.out.print("Choose an option: ");
+    public static void main(String[] args) {
+        List<User> users = UserRepository.loadUsers();
+        Scanner scanner = new Scanner(System.in);
+        boolean running = true;
 
-      int option = scanner.nextInt();
-      scanner.nextLine();
+        while (running) {
+            System.out.println("\n CRUD Menu:");
+            System.out.println("1. Create User");
+            System.out.println("2. Read Users");
+            System.out.println("3. Update User");
+            System.out.println("4. Delete User");
+            System.out.println("5. Exit");
+            System.out.print("Choose an option: ");
 
-      switch(option) {
-        case 1 -> {
-            System.out.print("type a user's name: ");
-            String name = scanner.nextLine();
-            System.out.print("type a user's age: ");
-            int age = scanner.nextInt();
-            users.add(new User(name, String.valueOf(age)));
-            System.out.println("User add with success");
-            }
-        case 2 -> {
-            System.out.println("\n Users registered");
-            for(int i =0; i < users.size(); i++) {
-                User user = users.get(i);
-                System.out.println(i + ": " + user.getName() + " (" + user.getAge() + " years old)");
-            } }
-        case 3 -> {
-            System.out.println("select an user to update");
-            int indexToUpdate = scanner.nextInt();
+            int option = scanner.nextInt();
             scanner.nextLine();
-            if (indexToUpdate >= 0 && indexToUpdate < users.size()) {
-                System.out.print("type the new user's name: ");
-                String newName = scanner.nextLine();
-                System.out.print("type the new user's age: ");
-                int newAge = scanner.nextInt();
-                User userToUpdate = users.get(indexToUpdate);
-                userToUpdate.setName(newName);
-                userToUpdate.setAge(String.valueOf(newAge));
-                System.out.println("User updated");
-            } else {
-                System.out.println("User not found");
-            } }
-        case 4 -> {
-            System.out.println("type user's index to delete");
-            int indexToRemove = scanner.nextInt();
-            if (indexToRemove >= 0 && indexToRemove <= users.size()) {
-                users.remove(indexToRemove);
-                System.out.println("user deleted");
-            } else {
-                System.out.println("user not found, try again");
-            } }
-        case 5 -> {
-            running = false;
-            System.out.println("Closing ...");
+
+            switch (option) {
+                case 1 -> {
+                    System.out.print("type a user's name: ");
+                    String name = scanner.nextLine();
+                    System.out.print("type a user's age: ");
+                    int age = scanner.nextInt();
+                    users.add(new User(name, String.valueOf(age)));
+                    UserRepository.saveUsers(users);
+                    System.out.println("User add with success");
+                }
+                case 2 -> {
+                    System.out.println("\n Users registered");
+                    for (int i = 0; i < users.size(); i++) {
+                        User user = users.get(i);
+                        System.out.println(i + ": " + user.getName() + " (" + user.getAge() + " years old)");
+                    }
+                }
+                case 3 -> {
+                    System.out.println("select an user to update");
+                    int indexToUpdate = scanner.nextInt();
+                    scanner.nextLine();
+                    if (indexToUpdate >= 0 && indexToUpdate < users.size()) {
+                        System.out.print("type the new user's name: ");
+                        String newName = scanner.nextLine();
+                        System.out.print("type the new user's age: ");
+                        int newAge = scanner.nextInt();
+                        User userToUpdate = users.get(indexToUpdate);
+                        userToUpdate.setName(newName);
+                        userToUpdate.setAge(String.valueOf(newAge));
+												UserRepository.saveUsers(users);
+                        System.out.println("User updated");
+                    } else {
+                        System.out.println("User not found");
+                    }
+                }
+                case 4 -> {
+                    System.out.println("type user's index to delete");
+                    int indexToRemove = scanner.nextInt();
+                    if (indexToRemove >= 0 && indexToRemove <= users.size()) {
+                        users.remove(indexToRemove);
+												UserRepository.saveUsers(users);
+                        System.out.println("user deleted");
+                    } else {
+                        System.out.println("user not found, try again");
+                    }
+                }
+                case 5 -> {
+                    running = false;
+                    System.out.println("Closing ...");
+                }
+                default ->
+                    System.out.println("choose another option");
             }
-        default -> System.out.println("choose another option");
-      }
+        }
     }
-  }
 }
--- UserRepository.java
+++ UserRepository.java
@@ -1,6 +1,3 @@
-package user;
-
-import java.awt.datatransfer.SystemFlavorMap;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
