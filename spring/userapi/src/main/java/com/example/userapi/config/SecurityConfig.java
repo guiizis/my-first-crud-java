afad3494a9b410ea02436bfbe8599b00f9d5ec19
guiziis <guigamoda@hotmail.com>
2025-05-11 18:15:00 -0300

    adds auth

---
+++ spring/userapi/src/main/java/com/example/userapi/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.example.userapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(auth
+                -> auth
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .anyRequest().authenticated()
+        )
+        .httpBasic(Customizer.withDefaults())
+        .csrf(csrf -> csrf.disable());
+
+      return httpSecurity.build();
+    }
+}
